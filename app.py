5150d287cc56eb1f4e6cc9cc9946550ca2b30826
Fonty McPython <joseantoniofontcia@gmail.com>
2019-03-09 12:24:32 +0100

    MongoDB + Log

--- app.py
+++ app.py
@@ -7,20 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('firstpage.html')
+    return render_template('firstpage.html', log = log[0])
 
 
 @app.route('/report/<hive_id>')
 def read_report(hive_id):
     print('Report from from: ' + str(hive_id))
+    return 'HOLA'
 
 
 @app.route('/notify/<hive_id>')
 def run_notification(hive_id):
     print('Notification from: ' + str(hive_id))
+    log.append('notification' + str(hive_id))
+    return 'HOLA'
 
 
+log = []
+client = MongoClient('localhost', 27017)
+db = client.test_database
 if __name__ == '__main__':
-    client = MongoClient('localhost', 27017)
-    db = client.test_database
     app.run()
